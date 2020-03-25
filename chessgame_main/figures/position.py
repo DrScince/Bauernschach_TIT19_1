bd794197296b635044cfa387aff07154e122f772
Donloran <tobias.spohn@googlemail.com>
2020-03-25 16:15:27 +0100

    Initial push of Position
    used to handle the position of the figures

---
+++ chessgame_main/figures/position.py
@@ -0,0 +1,36 @@
+""" Position Object to handle the position of a figure
+"""
+try:
+    from exceptions import OutOfBoundsException
+    from consts import GAME_SIZE
+    import sys
+except ImportError:
+    print("ImportError")
+    sys.exit()
+
+class Position():
+    """ Position Object to handle the position of a figure
+    """
+
+    def __init__(self, char_pos, number_pos):
+        if number_pos > GAME_SIZE or number_pos < 1:
+            raise OutOfBoundsException
+        if len(char_pos) == 1:
+            if ord(char_pos) > GAME_SIZE + 64 or ord(char_pos) < 64:
+                raise OutOfBoundsException
+        else:
+            raise Exception
+        self.pos_number = number_pos
+        self.pos_char = char_pos
+
+    def get_pos_number(self):
+        """
+        Return: the number part of the position
+        """
+        return self.pos_number
+
+    def get_pos_char(self):
+        """
+        Return: the char part of the position
+        """
+        return self.pos_char
