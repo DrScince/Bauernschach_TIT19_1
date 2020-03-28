d5e390bb2a5ae9172fc73c2aa5fab30684cfe343
Donloran <tobias.spohn@googlemail.com>
2020-03-28 20:44:36 +0100

    get_possible_moves (black/white) in eine funktion zusammengefasst

--- chessgame_main/chess_logik/pawn.py
+++ chessgame_main/chess_logik/pawn.py
@@ -18,65 +18,37 @@ class Pawn(Figure):
         self.__has_done_double_move = None
         self.__possible_moves_buffer = []
 
-    def __get_double_move(self):
-        return self.__has_done_double_move
-
-    def __get_possible_moves_black(self, field):
-        own_position = super().get_position()
-        straight_is_possible = True
-        double_straight_is_possible = False
-        if self.__has_done_double_move is None:
-            double_straight_is_possible = True
-        for figure in field:
-            if own_position.get_pos_number() - 1 == figure.get_position().get_pos_number():
-                if own_position.get_pos_char() == figure.get_position().get_pos_char():
-                    straight_is_possible = False
-                if ord(own_position.get_pos_char()) - 1 == ord(figure.get_position().get_pos_char()) and figure.get_color() == COLOR_WHITE:
-                    self.__possible_moves_buffer.append(Position(figure.get_position().get_pos_char(), figure.get_position().get_pos_number()))
-                if ord(own_position.get_pos_char()) + 1 == ord(figure.get_position().get_pos_char()) and figure.get_color() == COLOR_WHITE:
-                    self.__possible_moves_buffer.append(Position(figure.get_position().get_pos_char(), figure.get_position().get_pos_number()))
-            if double_straight_is_possible and own_position.get_pos_number() - 2 == figure.get_position().get_pos_number():
-                if own_position.get_pos_char() == figure.get_position().get_pos_char():
-                    double_straight_is_possible = False
-        if straight_is_possible:
-            self.__possible_moves_buffer.append(Position(own_position.get_pos_char(), own_position.get_pos_number() - 1))
-        if double_straight_is_possible and straight_is_possible:
-            self.__possible_moves_buffer.append(Position(own_position.get_pos_char(), own_position.get_pos_number() - 2))
-        return self.__possible_moves_buffer
-
-    def __get_possible_moves_white(self, field):
-        own_position = super().get_position()
-        straight_is_possible = True
-        double_straight_is_possible = False
-        if self.__has_done_double_move is None:
-            double_straight_is_possible = True
-        for figure in field:
-            if own_position.get_pos_number() + 1 == figure.get_position().get_pos_number():
-                if own_position.get_pos_char() == figure.get_position().get_pos_char():
-                    straight_is_possible = False
-                if ord(own_position.get_pos_char()) - 1 == ord(figure.get_position().get_pos_char()) and figure.get_color() == COLOR_BLACK:
-                    self.__possible_moves_buffer.append(Position(figure.get_position().get_pos_char(), figure.get_position().get_pos_number()))
-                if ord(own_position.get_pos_char()) + 1 == ord(figure.get_position().get_pos_char()) and figure.get_color() == COLOR_BLACK:
-                    self.__possible_moves_buffer.append(Position(figure.get_position().get_pos_char(), figure.get_position().get_pos_number()))
-            if double_straight_is_possible and own_position.get_pos_number() + 2 == figure.get_position().get_pos_number():
-                if own_position.get_pos_char() == figure.get_position().get_pos_char():
-                    double_straight_is_possible = False
-        if straight_is_possible:
-            self.__possible_moves_buffer.append(Position(own_position.get_pos_char(), own_position.get_pos_number() + 1))
-        if double_straight_is_possible and straight_is_possible:
-            self.__possible_moves_buffer.append(Position(own_position.get_pos_char(), own_position.get_pos_number() + 2))
-        return self.__possible_moves_buffer
-
     def get_possible_moves(self, field):
         """ gets all the possible moves of the Pawn
         Return:
             possible_moves {Position[]} -- if there are none the array is empty
         """
         self.__possible_moves_buffer = []
+        own_position = super().get_position()
+        straight_is_possible = True
+        double_straight_is_possible = False
         if super().get_color() == COLOR_BLACK:
-            self.__possible_moves_buffer = self.__get_possible_moves_black(field)
+            directional_factor = -1
         elif super().get_color() == COLOR_WHITE:
-            self.__possible_moves_buffer = self.__get_possible_moves_white(field)
+            directional_factor = 1
+        if self.__has_done_double_move is None:
+            double_straight_is_possible = True
+        for figure in field:
+            if own_position.get_pos_number() + directional_factor == figure.get_position().get_pos_number():
+                if own_position.get_pos_char() == figure.get_position().get_pos_char():
+                    straight_is_possible = False
+                if ord(own_position.get_pos_char()) - 1 == ord(figure.get_position().get_pos_char()) and figure.get_color() == COLOR_BLACK:
+                    self.__possible_moves_buffer.append(Position(figure.get_position().get_pos_char(), figure.get_position().get_pos_number()))
+                if ord(own_position.get_pos_char()) + 1 == ord(figure.get_position().get_pos_char()) and figure.get_color() == COLOR_BLACK:
+                    self.__possible_moves_buffer.append(Position(figure.get_position().get_pos_char(), figure.get_position().get_pos_number()))
+            if double_straight_is_possible and own_position.get_pos_number() + (directional_factor * 2) == figure.get_position().get_pos_number():
+                if own_position.get_pos_char() == figure.get_position().get_pos_char():
+                    double_straight_is_possible = False
+            #TODO en passant einfügen
+        if straight_is_possible:
+            self.__possible_moves_buffer.append(Position(own_position.get_pos_char(), own_position.get_pos_number() + directional_factor))
+        if double_straight_is_possible and straight_is_possible:
+            self.__possible_moves_buffer.append(Position(own_position.get_pos_char(), own_position.get_pos_number() + (directional_factor * 2)))
         return self.__possible_moves_buffer
 
     def do_move(self, new_position):
