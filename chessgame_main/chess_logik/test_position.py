5c8176ee7a70589b40cbee814af71dd3fea36ce3
Donloran <tobias.spohn@googlemail.com>
2020-04-01 13:07:42 +0200

    unittest für position.py geschrieben

---
+++ chessgame_main/chess_logik/test_position.py
@@ -0,0 +1,39 @@
+# pylint: disable=C
+import unittest
+import sys
+try:
+    from chess_logik.pawn import Position
+except ImportError as err:
+    print("ImportError"+str(err))
+    sys.exit()
+
+class PositionTest(unittest.TestCase):
+
+    def test_normal_init_and_getter(self):
+        for char in "ABCDEFGH":
+            for num in range(1,9):
+                assert self.assertEqual(Position(char,num).get_pos_number(), num)
+                assert self.assertEqual(Position(char,num).get_pos_char(), char)
+
+    def test_char_out_of_range(self):
+        for num in range(1,9):
+            assert self.assertEqual(Position("@",num).get_pos_number(), num)
+            assert self.assertEqual(Position("@",num).get_pos_char(), "Error:char")
+            assert self.assertEqual(Position("I",num).get_pos_number(), num)
+            assert self.assertEqual(Position("I",num).get_pos_char(), "Error:char")
+
+    def test_number_out_of_range(self):
+        for char in "ABCDEFGH":
+            assert self.assertEqual(Position(char,0).get_pos_number(), "Error:number")
+            assert self.assertEqual(Position(char,0).get_pos_char(), char)
+            assert self.assertEqual(Position(char,9).get_pos_number(), "Error:number")
+            assert self.assertEqual(Position(char,9).get_pos_char(), char)
+
+    def test_wrong_argument_types(self):
+        with self.assertRaises(Exception):
+            Position("A2", 2)
+        with self.assertRaises(Exception):
+            Position("A", "2")
+        with self.assertRaises(Exception):
+            Position(6, 2)
+            
