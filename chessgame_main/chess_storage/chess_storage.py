f1af2e91d808f124c4965476a0098f914c5753cc
Florian Christoph Veit <florian.christoph.veit@gmail.com>
2020-04-01 15:47:10 +0200

    ERROR_CODES and fix bug

--- chessgame_main/chess_storage/chess_storage.py
+++ chessgame_main/chess_storage/chess_storage.py
@@ -32,14 +32,14 @@ class ChessStorage:
             current_game {matchfield} -- the current game to save
             overwrite {bool} -- True: overwrite the file, False: do nothing
         Returns:
-            ErrorCode -- 0 success, 2 file exists
+            ERROR_CODE -- SUCCESSFULL, FILE_EXIST
         """
         __dir_game = os.path.join(self.__dir_game_saves, str(file_name))
         if os.path.isfile(__dir_game):
             if overwrite:
                 return self.__write_file(__dir_game, current_game)
             else:
-                return consts.FILE_EXIST
+                return consts.ERROR_CODES["FILE_EXIST"]
         else:
             return self.__write_file(__dir_game, current_game)
 
@@ -48,7 +48,7 @@ class ChessStorage:
         Arguments:
             file_name {string} -- name frome file to load
         Returns:
-            matchfield -- returns the loaded matchfield or None
+            matchfield -- returns the loaded matchfield or ERROR_CODE -- FILE_EXIST
         """
         __dir_load_game = os.path.join(self.__dir_game_saves, str(file_name))
         try:
@@ -56,7 +56,7 @@ class ChessStorage:
                 __new_matchfield = pickle.load(__new_game)
                 return __new_matchfield
         except IOError:
-            return None
+            return consts.ERROR_CODES["NO_FILE_EXIST"]
 
     def get_all_games(self):
         """list all files in ../games
@@ -76,7 +76,7 @@ class ChessStorage:
             log_info {} -- info to log (converts to string)
             append {bool} -- True: append the file, False: do nothing
         Returns:
-            ErrorCode -- 0 success, 2 file exists
+            ERROR_CODE -- SUCCESSFULL, FILE_EXIST
         """
         __log_name = str(file_name) + "_log.txt"
         __dir_game_log = os.path.join(self.__dir_game_log, __log_name)
@@ -84,7 +84,7 @@ class ChessStorage:
             if append:
                 return self.__log_append(__dir_game_log, log_info)
             else:
-                return consts.FILE_EXIST
+                return consts.ERROR_CODES["FILE_EXIST"]
         else:
             return self.__log_append(__dir_game_log, log_info)
 
@@ -94,12 +94,12 @@ class ChessStorage:
             file_path {string} -- must be a os path
             current_game {matchfield} -- the current game to save
         Returns:
-            ErrorCode -- 0 Successs
+            ERROR_CODE -- SUCCESSFULL
         """
         assert isinstance(file_path, str), "file_path is not a string"
         with open(file_path, 'wb') as __old_game:
             pickle.dump(current_game, __old_game)
-            return consts.SUCCESSFULL
+            return consts.ERROR_CODES["SUCCESSFULL"]
 
     def __log_append(self, file_path, log_info):
         """Append logfiles
@@ -107,7 +107,7 @@ class ChessStorage:
             file_path {string} -- must be a os path
             log_info {} -- info to log (converts to string)
         Returns:
-            ErrorCode -- 0 Successs
+            ERROR_CODE -- SUCCESSFULL
         """
         assert isinstance(file_path, str), "file_path is not a string"
         with open(file_path, 'a', encoding="utf8") as __log_game:
@@ -115,7 +115,7 @@ class ChessStorage:
                 __len_list = len(log_info)
                 for i in range(__len_list):
                     __log_game.writelines([str(log_info[i]), "\n"])
-                return consts.SUCCESSFULL
+                return consts.ERROR_CODES["SUCCESSFULL"]
             else:
                 __log_game.writelines([str(log_info), "\n"])
-                return consts.SUCCESSFULL
+                return consts.ERROR_CODES["SUCCESSFULL"]
