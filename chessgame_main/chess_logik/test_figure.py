088ed93c74dbe8689db58b3276d636b9201f6168
Donloran <tobias.spohn@googlemail.com>
2020-04-05 10:55:15 +0200

    Mehr unit tests

--- chessgame_main/chess_logik/test_figure.py
+++ chessgame_main/chess_logik/test_figure.py
@@ -4,11 +4,22 @@ import sys
 try:
     from chess_logik.position import Position
     from chess_logik.figure import Figure
+    from chess_logik import consts
 except ImportError as err:
     print("ImportError "+str(err))
     sys.exit()
 
 class FigureTest(unittest.TestCase):
+
+    def test_normal_init_and_getter(self):
+        tmp_pos = Position("A", 2)
+        tmp = Figure(consts.COLOR_BLACK, tmp_pos)
+        self.assertEqual(tmp.get_color, consts.COLOR_BLACK)
+        self.assertEqual(tmp.get_position, tmp_pos)
+
+    def test_error_codes(self):
+        self.assertEqual(Figure(consts.COLOR_WHITE, None), consts.ERROR_CODES["None"])
+        self.assertEqual(Figure(consts.COLOR_WHITE, Position("A", 2).do_move(Position("A", 3))), consts.ERROR_CODES["Success"])
 
     def test_wrong_argument_types(self):
         with self.assertRaises(Exception):
