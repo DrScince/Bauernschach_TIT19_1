79d22123cff91c5f4c27f00e71cf72e8084e79d4
Frank Rübenkönig <frankruebenkoenig@gmail.com>
2020-04-05 09:36:17 +0200

    Auto stash before merge of "chess_main" and "origin/Chess-Bot"

--- chessgame_main/test_game.py
+++ chessgame_main/test_game.py
@@ -3,6 +3,7 @@
 #TODO docstring
 
 import unittest
+import builtins
 import sys
 
 try:
@@ -35,7 +36,6 @@ class GameTest(unittest.TestCase):
         unittest {[type]} -- [description]
     """
     #TODO docstring
-
     __test_game = game.ActiveGame("Test1", "Test2", "Test_Game", False, chess_storage.ChessStorage())
 
     def test_000_init(self):
@@ -49,20 +49,44 @@ class GameTest(unittest.TestCase):
         """[summary]
         """
         #TODO docstring
+        self.__run_test_game = game.ActiveGame("Test1", "Test2", "Test_Game", False, chess_storage.ChessStorage())
+        with captured_std() as (out, err, inp):
+            self.__run_test_game.run_game()
+        out = inp.write("S").strip()
 
     def test_002_update_game(self):
-        """[summary]
+        """Test the update game function
         """
+        with captured_std() as (out, err, inp):
+            self.__test_game._ActiveGame__update_game()
+        output = out.getvalue().strip()
+        accepted_output = "Schachautomat\t\t\n\n\t\tNeues Spiel(N)\t\tSpeichern(S)\tLaden(L)\t\tSpiel Beenden(B)\n\t\t________________________________________"
+        accepted_output = accepted_output + "________________________________________\n\n\t\t\t\t    A    B    C    D    E    F    G    H\n\n\t\t\t       \x1b"
+        accepted_output = accepted_output + "[6;34;47m8\x1b[0;30;47m    ◻    ◼    ◻    ◼    ◻    ◼    ◻    ◼    \x1b[6;34;47m8\x1b[0;30;47m\n\t\t\t       \x1b"
+        accepted_output = accepted_output + "[6;34;47m7\x1b[0;30;47m    ♟    ♟    ♟    ♟    ♟    ♟    ♟    ♟    \x1b[6;34;47m7\x1b[0;30;47m\n\t\t\t       \x1b"
+        accepted_output = accepted_output + "[6;34;47m6\x1b[0;30;47m    ◻    ◼    ◻    ◼    ◻    ◼    ◻    ◼    \x1b[6;34;47m6\x1b[0;30;47m\n\t\t\t       \x1b"
+        accepted_output = accepted_output + "[6;34;47m5\x1b[0;30;47m    ◼    ◻    ◼    ◻    ◼    ◻    ◼    ◻    \x1b[6;34;47m5\x1b[0;30;47m\t\t\x1b[0;30;47m\n"
+        accepted_output = accepted_output + "\t\t\t       \x1b[6;34;47m4\x1b[0;30;47m    ◻    ◼    ◻    ◼    ◻    ◼    ◻    ◼    \x1b[6;34;47m4\x1b[0;30;47m\t"
+        accepted_output = accepted_output + "\t\x1b[0;30;47m\n\t\t\t       \x1b[6;34;47m3\x1b[0;30;47m    ◼    ◻    ◼    ◻    ◼    ◻    ◼    ◻    \x1b[6;34;47"
+        accepted_output = accepted_output + "m3\x1b[0;30;47m\n\t\t\t       \x1b[6;34;47m2\x1b[0;30;47m    ♙    ♙    ♙    ♙    ♙    ♙    ♙    ♙    \x1b[6;34;47"
+        accepted_output = accepted_output + "m2\x1b[0;30;47m\n\t\t\t       \x1b[6;34;47m1\x1b[0;30;47m    ◼    ◻    ◼    ◻    ◼    ◻    ◼    ◻    \x1b[6;34;47"
+        accepted_output = accepted_output + "m1\x1b[0;30;47m\n\n\t\t\t\t    A    B    C    D    E    F    G    H\n\n\t\t______________________________________"
+        accepted_output = accepted_output + "__________________________________________"
+        self.assertEqual(output, accepted_output)
         #TODO docstring
 
     def test_003_fill_default_game(self):
         """[summary]
         """
+        __gamefield_test = self.__test_game._ActiveGame__fill_default_game()
+        self.assertIsInstance(self.__test_game, list)
         #TODO docstring
 
     def test_004_fill_game_field(self):
         """[summary]
         """
+        __gamefield_test = self.__test_game._ActiveGame__fill_game_field()
+        self.assertIsInstance(self.__test_game, list)
         #TODO docstring
 
     def test_005_print_game_all(self):
@@ -164,27 +188,34 @@ class GameTest(unittest.TestCase):
         """[summary]
         """
         #TODO docstring
+        __test_name = self.__test_game.get_game_name()
+        self.assertEqual(__test_name, "Test_Game")
 
     def test_014_get_playername_one(self):
         """[summary]
         """
         #TODO docstring
+        __test_name_1 = self.__test_game.get_playername_one()
+        self.assertEqual(__test_name_1, "Test1")
 
     def test_015_get_playername_two(self):
         """[summary]
         """
         #TODO docstring
+        __test_name_2 = self.__test_game.get_playername_two()
+        self.assertEqual(__test_name_2, "Test2")
 
     def test_016_get_play_against_bot(self):
         """[summary]
         """
         #TODO docstring
+        __test_name = self.__test_game.get_play_against_bot()
+        self.assertEqual(__test_name, False)
 
     def test_999_remove_testfiles(self):
         """[summary]
         """
         #TODO docstring
-
 # Needed to run without console command
 # if __name__ == '__main__':
 #     unittest.main()
