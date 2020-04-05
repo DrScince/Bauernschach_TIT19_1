dd0d8da2fd2a3af29015f7c81625366c3d641028
Florian Christoph Veit <florian.christoph.veit@gmail.com>
2020-04-05 21:31:42 +0200

    Pylint warnings

--- chessgame_main/test_main.py
+++ chessgame_main/test_main.py
@@ -1,3 +1,5 @@
+"""test main.py
+"""
 import unittest
 from unittest.mock import patch
 import sys
@@ -21,9 +23,8 @@ except ImportError:
 
 @contextmanager
 def captured_std():
-    """[summary]
+    """To capture the terminal output
     """
-    #TODO docstring
     new_out, new_err, new_in = io.StringIO(), io.StringIO(), io.StringIO()
     old_out, old_err, old_in = sys.stdout, sys.stderr, sys.stdin
     try:
@@ -34,18 +35,18 @@ def captured_std():
 
 
 class MainTest(unittest.TestCase):
+    """Tests the class MainTest
+    """
     def test_000_init(self):
         """no init needed because main is no class
         """
 
     @patch('builtins.input')
     def test_001_main(self, mock_input):
-        """"[summary]
-          
-        Arguments:
-            self {[type]} -- [description]
+        """"Test the main function
         """
-        mock_input.side_effect = ["l", consts.GAME_MODE["QUIT"]]
+        mock_input.side_effect = ["l",
+                                  consts.GAME_MODE["QUIT"]]
         main()
         mock_input.side_effect = ["n",
                                   "n",
@@ -65,31 +66,112 @@ class MainTest(unittest.TestCase):
                                   consts.GAME_MODE["QUIT"],
                                   consts.GAME_MODE["QUIT"]]
         main()
-        mock_input.side_effect = ["n", "n", "Test_Game_Main", "Test1", "Test2", consts.GAME_MODE["NEW_GAME"], consts.GAME_MODE["QUIT"], consts.GAME_MODE["QUIT"], consts.GAME_MODE["QUIT"]]
+        mock_input.side_effect = ["n",
+                                  "n",
+                                  "Test_Game_Main",
+                                  "Test1",
+                                  "Test2",
+                                  consts.GAME_MODE["NEW_GAME"],
+                                  consts.GAME_MODE["QUIT"],
+                                  consts.GAME_MODE["QUIT"],
+                                  consts.GAME_MODE["QUIT"]]
         main()
-        mock_input.side_effect = ["n", "n", "Test_Game_Main", "Test1", "Test2", consts.GAME_MODE["SAVE"], consts.GAME_MODE["QUIT"], consts.GAME_MODE["QUIT"], consts.GAME_MODE["QUIT"], consts.GAME_MODE["QUIT"]]
+        mock_input.side_effect = ["n",
+                                  "n",
+                                  "Test_Game_Main",
+                                  "Test1",
+                                  "Test2",
+                                  consts.GAME_MODE["SAVE"],
+                                  consts.GAME_MODE["QUIT"],
+                                  consts.GAME_MODE["QUIT"],
+                                  consts.GAME_MODE["QUIT"],
+                                  consts.GAME_MODE["QUIT"]]
         main()
-        mock_input.side_effect = ["n", "n", "Test_Game_Main", "Test1", "Test2", consts.GAME_MODE["SAVE"], consts.GAME_MODE["SAVE_NEW"], consts.GAME_MODE["QUIT"], consts.GAME_MODE["QUIT"], consts.GAME_MODE["QUIT"], consts.GAME_MODE["QUIT"]]
+        mock_input.side_effect = ["n",
+                                  "n",
+                                  "Test_Game_Main",
+                                  "Test1",
+                                  "Test2",
+                                  consts.GAME_MODE["SAVE"],
+                                  consts.GAME_MODE["SAVE_NEW"],
+                                  consts.GAME_MODE["QUIT"],
+                                  consts.GAME_MODE["QUIT"],
+                                  consts.GAME_MODE["QUIT"],
+                                  consts.GAME_MODE["QUIT"]]
         main()
-        mock_input.side_effect = ["n", "n", "Test_Game_Main__1", "Test1", "Test2", consts.GAME_MODE["SAVE"], consts.GAME_MODE["SAVE_NEW"], consts.GAME_MODE["QUIT"], consts.GAME_MODE["QUIT"], consts.GAME_MODE["QUIT"], consts.GAME_MODE["QUIT"]]
+        mock_input.side_effect = ["n",
+                                  "n",
+                                  "Test_Game_Main__1",
+                                  "Test1",
+                                  "Test2",
+                                  consts.GAME_MODE["SAVE"],
+                                  consts.GAME_MODE["SAVE_NEW"],
+                                  consts.GAME_MODE["QUIT"],
+                                  consts.GAME_MODE["QUIT"],
+                                  consts.GAME_MODE["QUIT"],
+                                  consts.GAME_MODE["QUIT"]]
         main()
-        mock_input.side_effect = ["n", "n", "Test_Game_Main", "Test1", "Test2", consts.GAME_MODE["QUIT"], consts.GAME_MODE["SAVE_NEW"], consts.GAME_MODE["QUIT"]]
+        mock_input.side_effect = ["n",
+                                  "n",
+                                  "Test_Game_Main",
+                                  "Test1",
+                                  "Test2",
+                                  consts.GAME_MODE["QUIT"],
+                                  consts.GAME_MODE["SAVE_NEW"],
+                                  consts.GAME_MODE["QUIT"]]
         main()
-        mock_input.side_effect = ["n", "n", "Test_Game_Main", "Test1", "Test2", consts.GAME_MODE["NEW_GAME"], "1", consts.GAME_MODE["QUIT"], consts.GAME_MODE["QUIT"], consts.GAME_MODE["QUIT"]]
+        mock_input.side_effect = ["n",
+                                  "n",
+                                  "Test_Game_Main",
+                                  "Test1",
+                                  "Test2",
+                                  consts.GAME_MODE["NEW_GAME"],
+                                  "1",
+                                  consts.GAME_MODE["QUIT"],
+                                  consts.GAME_MODE["QUIT"],
+                                  consts.GAME_MODE["QUIT"]]
         main()
-        mock_input.side_effect = ["n", "n", "Test_Game_Main", "Test1", "Test2", consts.GAME_MODE["NEW_GAME"], "2", "n", "Test_Game_Main", "Test1", "Test2", consts.GAME_MODE["QUIT"], consts.GAME_MODE["QUIT"], consts.GAME_MODE["QUIT"]]
+        mock_input.side_effect = ["n",
+                                  "n",
+                                  "Test_Game_Main",
+                                  "Test1",
+                                  "Test2",
+                                  consts.GAME_MODE["NEW_GAME"],
+                                  "2",
+                                  "n",
+                                  "Test_Game_Main",
+                                  "Test1",
+                                  "Test2",
+                                  consts.GAME_MODE["QUIT"],
+                                  consts.GAME_MODE["QUIT"],
+                                  consts.GAME_MODE["QUIT"]]
         main()
-        mock_input.side_effect = ["n", "n", "Test_Game_Main", "Test1", "Test2", consts.GAME_MODE["NEW_GAME"], "3", "1", consts.GAME_MODE["QUIT"], consts.GAME_MODE["QUIT"], consts.GAME_MODE["QUIT"]]
+        mock_input.side_effect = ["n",
+                                  "n",
+                                  "Test_Game_Main",
+                                  "Test1", "Test2",
+                                  consts.GAME_MODE["NEW_GAME"],
+                                  "3",
+                                  "1",
+                                  consts.GAME_MODE["QUIT"],
+                                  consts.GAME_MODE["QUIT"],
+                                  consts.GAME_MODE["QUIT"]]
         main()
-
-        mock_input.side_effect = ["l", "1", "H2", "H4", consts.GAME_MODE["QUIT"], consts.GAME_MODE["QUIT"], consts.GAME_MODE["QUIT"]]
+        mock_input.side_effect = ["l",
+                                  "1",
+                                  "H2",
+                                  "H4",
+                                  consts.GAME_MODE["QUIT"],
+                                  consts.GAME_MODE["QUIT"],
+                                  consts.GAME_MODE["QUIT"]]
         main()
-        mock_input.side_effect = [" " , consts.GAME_MODE["QUIT"]]
+        mock_input.side_effect = [" ",
+                                  consts.GAME_MODE["QUIT"]]
         main()
 
     @patch('builtins.input')
     def test_002_print_welcome_screen(self, mock_input):
-        """[summary]
+        """Test the function print_welcome_screen
         """
         mock_input.side_effect = consts.GAME_MODE["NEW_GAME"]
         result = print_welcome_screen()
@@ -100,13 +182,10 @@ class MainTest(unittest.TestCase):
         mock_input.side_effect = consts.GAME_MODE["QUIT"]
         result = print_welcome_screen()
         self.assertEqual(result, consts.GAME_MODE["QUIT"])
-        
+
     @patch('builtins.input')
     def test_003_get_load_game(self, mock_input):
-        """[summary]
-          
-        Arguments:
-            self {[type]} -- [description]
+        """Test the function get_load_game
         """
         mock_input.side_effect = ["1"]
         result = get_load_game(["Game1", "Game2", "Game3"])
@@ -120,12 +199,12 @@ class MainTest(unittest.TestCase):
         mock_input.side_effect = ["1"]
         result = get_load_game([])
         self.assertEqual(result, consts.ERROR_CODES["NO_GAME_TO_LOAD"])
-        #TODO 
 
     @patch('builtins.input')
     def test_004_check_new_game(self, mock_input):
-        """[summary]
+        """Test the function get_load_game
         """
+        # pylint: disable = unused-variable
         test_storage = ChessStorage()
         test_game = ActiveGame("Test1", "Test2", "Test_Game", None, test_storage)
         mock_input.side_effect = consts.GAME_MODE["SAVE"]
@@ -134,7 +213,7 @@ class MainTest(unittest.TestCase):
         output = out.getvalue().strip()
         accept_output = 'Spiel wurde noch nicht gepspeichert\n\t\t\t\t\t(S)\tSpiel Speichern\n\t\t\t\t\t(B)\tohne Speichern fortfahren'
         self.assertEqual(output, accept_output)
-
+        #
         mock_input.side_effect = consts.GAME_MODE["SAVE_NEW"]
         with captured_std() as (out, err, inp):
             check_game_saved(test_game, test_storage)
@@ -142,7 +221,7 @@ class MainTest(unittest.TestCase):
         accept_output = 'Spiel wurde bereits gespeichert\n\t\t\t\t\tSoll der Spielstand überschrieben werden\n\t\t\t\t\t(S)\tSpiel Überschreiben\n\t\t\t\t\t(S'
         accept_output = accept_output + 'N)\tals neues Spiel Speichern\n\t\t\t\t\t(B)\tohne Speichern fortfahren'
         self.assertEqual(output, accept_output)
-
+        #
         mock_input.side_effect = consts.GAME_MODE["QUIT"]
         with captured_std() as (out, err, inp):
             check_game_saved(test_game, test_storage)
@@ -150,10 +229,11 @@ class MainTest(unittest.TestCase):
         accept_output = 'Spiel wurde bereits gespeichert\n\t\t\t\t\tSoll der Spielstand überschrieben werden\n\t\t\t\t\t(S)\tSpiel Überschreiben\n\t\t\t\t\t(S'
         accept_output = accept_output + 'N)\tals neues Spiel Speichern\n\t\t\t\t\t(B)\tohne Speichern fortfahren'
         self.assertEqual(output, accept_output)
-    
+        # pylint: enable = unused-variable
+
     @patch('builtins.input')
     def test_005_set_new_game(self, mock_input):
-        """[summary]
+        """Test the function set_new_game
         """
         test_storage = ChessStorage()
         mock_input.side_effect = ["Schnitzel", "n", "Test_Game1", "Test1", "Test2"]
@@ -164,11 +244,11 @@ class MainTest(unittest.TestCase):
         self.assertIsInstance(result, ActiveGame)
 
     def test_006_quit_game(self):
-        """[summary]
+        """est the function quit_game
         """
         result = quit_game()
         self.assertEqual(result, False)
-        
+
     def test_999_remove_testfiles(self):
         """Remove all created files
         """
@@ -210,7 +290,6 @@ class MainTest(unittest.TestCase):
             __list_files = os.listdir(__dir_game_saves)
             if len(__list_files) == 0:
                 os.removedirs(__dir_game_saves)
-      
 
 # Needed to run without console command
 # if __name__ == '__main__':
