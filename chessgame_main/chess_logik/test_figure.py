a7ea849f090e1e3f6b56d8afd384ac43772f1573
Donloran <tobias.spohn@googlemail.com>
2020-04-05 12:25:59 +0200

    more unit tests

--- chessgame_main/chess_logik/test_figure.py
+++ chessgame_main/chess_logik/test_figure.py
@@ -14,12 +14,11 @@ class FigureTest(unittest.TestCase):
     def test_normal_init_and_getter(self):
         tmp_pos = Position("A", 2)
         tmp = Figure(consts.COLOR_BLACK, tmp_pos)
-        self.assertEqual(tmp.get_color, consts.COLOR_BLACK)
-        self.assertEqual(tmp.get_position, tmp_pos)
+        self.assertEqual(tmp.get_color(), consts.COLOR_BLACK)
+        self.assertEqual(tmp.get_position(), tmp_pos)
 
     def test_error_codes(self):
-        self.assertEqual(Figure(consts.COLOR_WHITE, None), consts.ERROR_CODES["None"])
-        self.assertEqual(Figure(consts.COLOR_WHITE, Position("A", 2).do_move(Position("A", 3))), consts.ERROR_CODES["Success"])
+        self.assertEqual(Figure(consts.COLOR_WHITE, Position("A", 2)).do_move(Position("A", 3)), consts.ERROR_CODES["Success"])
 
     def test_wrong_argument_types(self):
         with self.assertRaises(Exception):
